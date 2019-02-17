f9a51255441de64545e0fc2973addcd7c2a0b014
Ryan <rjpadil1@uci.edu>
2019-02-16 22:08:24 -0800

    Attempting to merge branches

--- noteify-fe/src/App.js
+++ noteify-fe/src/App.js
@@ -1,24 +1,25 @@
-import React, {Component} from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import BoardImage from "./BoardImage/BoardImage";
-import {Container} from "semantic-ui-react";
+import React, { Component } from 'react';
+import './App.css';
+import {Container} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.css'
+import {BrowserRouter, Route} from "react-router-dom";
+import Student from './StudentView/Student'
+import StudentLogin from './Login/StudentLogin'
+import ProfessorLogin from './Login/ProfessorLogin';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Container>
-          <BoardImage
-            src={'http://ec2-54-193-114-159.us-west-1.compute.amazonaws.com:5000/static/img-16-02-22_46_00.jpg'}
-            header={'First Post'}
-            posted={'Posted Today'}
-            description={'This photo was taken when everyone was less tired'}
-            tags={"Mango"}
-          />
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <Container>
+                    <Route path={'/'} exact component={StudentLogin}/>
+                    <Route path={'/profLogin'} exact component={ProfessorLogin}/>
+                    <Route path={'/student'} component={Student}/>
+                </Container>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
---
+++ noteify-fe/src/StudentView/Search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Container} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.css'
+
+class Search extends Component {
+    render() {
+        return (
+            <Container>
+                <p>Search</p>
+            </Container>
+        );
+    }
+}
+
+export default Search;
